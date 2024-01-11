2d55b5a22068e6fd6386062253ce37549643e483
george9907 <109024372+george9907@users.noreply.github.com>
2024-01-11 22:14:07 +0100

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+
+changeUsername: function(newName) {
+    profile.username = newName;
+},
+  updatePlayTime: function(hours) {
+   profile.playTime += hours;
+},
+getInfo: function () {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
+};
+
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
